decc4e8616a38ad96497ee4403aba6784a118a79
Eguchi-Masayuki <16ec026@ms.dendai.ac.jp>
2018-08-10 11:10:57 +0900

    10進数を2進数に変換する

---
+++ 10to2/10to2/_10to2.py
@@ -0,0 +1,31 @@
+##二進数に変換
+def cardConvR(x):
+    a=[]
+    while x!=0:
+        a.append(int(x%2))
+        x=x//2
+    return a
+
+def main():
+    print("10進数を基底変換します。")
+    print("変換する非負の整数:",end="")
+    try:
+        num=int(input()) 
+        if num<0:
+            print("正の整数を入力してください。\n")
+            main()
+        else:
+            print("2進数では",end="")
+            ##リストを逆順にソート
+            ans=cardConvR(num)[::-1]
+            for i in ans:
+                print(i,end="")
+            print("です。")
+
+    ##例外処理
+    except:
+        print("非負の整数を入力してください。\n")
+        main()
+
+##実行部分
+main()
